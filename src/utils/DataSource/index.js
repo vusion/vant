550e6a5dfae3e7010ea8dc3f076b6707769064c1
YufJi <18012261618@126.com>
2023-06-26 17:12:48 +0800

    feat: 选择器支持数据源

--- src/utils/DataSource/index.js
+++ src/utils/DataSource/index.js
@@ -333,7 +333,7 @@ const VueDataSource = Vue.extend({
             params.start = params.paging.offset;
             params.size = params.paging.size;
           }
-          if (this.sorting && this.sorting.field) {
+          if (this.remoteSorting && this.sorting && this.sorting.field) {
             params.sort = params.sorting.field;
             params.order = params.sorting.order;
           }
