50b08a031f78c88c5d3711061e81b174dc2a9a32
liuyiqi <liuyiqi02@corp.netease.com>
2023-07-10 11:17:34 +0800

    feat: h5 增加自由布局组件

---
+++ src-vusion/components/absolute-layout/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div :class="$style.root" v-on="$listeners" vusion-slot-name="default">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'van-absolute-layout',
+};
+</script>
+
+<style module>
+.root {
+    position: relative;
+    width: 100%;
+    height: 300px;
+}
+
+.root>* {
+    position: absolute !important;
+}
+/* 在编辑器里禁用 transition，防止位置尺寸变化时候出现延迟 */
+.root>[vusion-node-path]{
+    transition: none !important;
+}
+</style>
+
