60729458b9cdb4a8e098bf669421df42225479c7
御风 <18012261618@126.com>
2023-09-07 18:56:43 +0800

    feature: 时间日期支持区间

--- src/datetime-picker/demo/index.vue
+++ src/datetime-picker/demo/index.vue
@@ -1,16 +1,77 @@
 <template>
   <demo-section>
     <demo-block card title="受控联动绑定">
-      <van-datetime-picker :ref="`datetime_picker1`" type="datetime" title="选择完整时间" input-align="left" :value.sync="variable1">
-            <template #title>
-                <van-text :ref="`text1`" text="标题"></van-text>
+      <div>value: {{ date }}</div>
+      <van-datetime-picker
+        :isNew="false"
+        :ref="`datetime_picker1`"
+        type="date"
+        title="选择date"
+        input-align="left"
+        :value.sync="date"
+        :startValue.sync="startDate"
+        :endValue.sync="endDate"
+        :range="true">
+          <template #top-cancel>
+            <van-iconv name="left-arrow" icotype="only"> </van-iconv>
+          </template>
+          <template #title>
+            <van-text text="顶部标题"></van-text>
+          </template>
+          <template #bottom-cancel>
+            <van-button
+              type="info_secondary"
+              size="normal"
+              text="取消"
+              squareroud="round"
+            ></van-button>
+          </template>
+          <template #bottom-confirm>
+            <van-button
+              type="info"
+              size="normal"
+              text="确认"
+              squareroud="round"
+            ></van-button>
           </template>
       </van-datetime-picker>
-      <van-datetime-picker :ref="`datetime_picker2`" type="datetime" title="选择完整时间" input-align="left" :value.sync="variable1">
-            <template #title>
-                <van-text :ref="`text2`" text="标题"></van-text>
+      <van-button @click="clear">clear</van-button>
+      <div>time: {{ time }}</div>
+      <van-datetime-picker
+        :isNew="false"
+        :ref="`datetime_picker1`"
+        type="time"
+        title="选择time"
+        input-align="left"
+        :value.sync="time"
+        :startValue.sync="startTime"
+        :endValue.sync="endTime"
+        :range="true">
+          <template #top-cancel>
+            <van-iconv name="left-arrow" icotype="only"> </van-iconv>
+          </template>
+          <template #title>
+            <van-text text="顶部标题"></van-text>
+          </template>
+          <template #bottom-cancel>
+            <van-button
+              type="info_secondary"
+              size="normal"
+              text="取消"
+              squareroud="round"
+            ></van-button>
+          </template>
+          <template #bottom-confirm>
+            <van-button
+              type="info"
+              size="normal"
+              text="确认"
+              squareroud="round"
+            ></van-button>
           </template>
       </van-datetime-picker>
+
+      <van-button @click="clear">clear</van-button>
     </demo-block>
 
     <demo-block card :title="t('yearMonthType')">
@@ -96,6 +157,13 @@ export default {
       minDate: new Date(2020, 0, 1),
       maxDate: "2020-12-12 12:12:12" || new Date(2025, 10, 1),
       date: "2021-01-13 00:00:00" || new Date(2021, 0, 17),
+      startDate: undefined,
+      endDate: new Date(2024, 1, 1),
+
+      time: undefined,
+      startTime: undefined,
+      endTime: undefined,
+
       value: {
         date: "2021-01-13 00:00:00" || new Date(2021, 0, 17),
         time: '12:00',
@@ -133,6 +201,14 @@ export default {
     },
     changeDate() {
       this.date = '2023-09-01'
+    },
+    clear() {
+      this.date = null
+      this.startDate = null
+      this.endDate = null
+      this.time = null
+      this.startTime = null
+      this.endTime = null
     }
   },
 };
