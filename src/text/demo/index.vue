e6cb88e176e43c3f1275a38a324ab12767aa05b7
御风 <18012261618@126.com>
2023-09-20 15:50:34 +0800

    feat: 文本组件支持格式化

---
+++ src/text/demo/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <demo-section>
+    <demo-block title="数字格式化">
+        <div>value: {{ value.number }}</div>
+        <van-text
+          :text="value.number"
+          :format="{ type: 'number', options: { 'decimal': { 'places': '', 'omit': true }, 'thousandths': false, 'percentage': false, 'unit': { 'type': 'prefix', 'value': '' } } }"></van-text>
+    </demo-block>
+    <demo-block title="日期格式化">
+        <div>value: {{ value.date }}</div>
+        <van-text
+          :text="value.date"
+          :format="{ type: 'date', options: { date: 'YYYY-MM-DD' } }"></van-text>
+    </demo-block>
+  </demo-section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: {
+        number: '123213123.001223',
+        date: '2023-09-20T07:41:45.174Z',
+      }
+    }
+  },
+}
+</script>
