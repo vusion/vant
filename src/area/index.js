ecd511a3bdd5d74c7e597458b0e697dfa72a9192
ncqwer <zhujianshi123@gmail.com>
2023-10-08 10:06:47 +0800

    Merge pull request #168 from vusion/feature/ActionSlot-ui-change-230920-hanshijie
    Feature/action slot UI change 230920 hanshijie

--- src/area/index.js
+++ src/area/index.js
@@ -5,8 +5,10 @@ import { pickerProps } from '../picker/shared';
 import Picker from '../picker';
 import Popup from '../popup';
 import Field from '../field';
+import { EmptyCol } from '../emptycol';
 
 import { FieldMixin } from '../mixins/field';
+import { EventSlotCommandProvider } from '../mixins/EventSlotCommandProvider';
 
 const [createComponent, bem] = createNamespace('area');
 
@@ -32,7 +34,7 @@ function pickSlots(instance, keys) {
 }
 
 export default createComponent({
-  mixins: [FieldMixin],
+  mixins: [FieldMixin, EventSlotCommandProvider(['confirm', 'cancel'])],
   props: {
     ...pickerProps,
     value: String,
@@ -51,14 +53,18 @@ export default createComponent({
     },
     converter: {
       type: String,
-      default: 'json'
+      default: 'json',
     },
     labelField: {
       type: String,
-      default: ''
+      default: '',
     },
     inputAlign: String,
-    closeOnClickOverlay: Boolean
+    closeOnClickOverlay: Boolean,
+    isNew: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   data() {
@@ -108,7 +114,7 @@ export default createComponent({
         this.setValues();
         this.setTitle();
       },
-      immediate: true
+      immediate: true,
     },
 
     areaListprop: {
@@ -116,11 +122,17 @@ export default createComponent({
       handler: 'setValues',
     },
 
+    isNew() {
+      this.$nextTick(function () {
+        this.setValues(true);
+      });
+    },
+
     columnsNum() {
       this.$nextTick(() => {
         this.setValues();
       });
-    }
+    },
   },
 
   mounted() {
@@ -129,26 +141,56 @@ export default createComponent({
   },
 
   methods: {
+    designerDbControl() {
+      this.valuepopup = true;
+      this.$refs.popforcas.togglePModal();
+      this.$nextTick(function () {
+        this.setValues(true);
+      });
+    },
+    designerClose() {
+      if (window.parent && this?.$attrs?.['vusion-node-path']) {
+        window.parent?.postMessage(
+          {
+            protocol: 'vusion',
+            sender: 'helper',
+            type: 'send',
+            command: 'setPopupStatusInfo',
+            args: [
+              {
+                nodePath: this?.$attrs?.['vusion-node-path'],
+                visible: false,
+              },
+            ],
+          },
+          '*'
+        );
+      }
+      this.$refs.popforcas.togglePModal();
+      this.valuepopup = false;
+    },
     ifDesigner() {
       return this.$env && this.$env.VUE_APP_DESIGNER;
     },
     setTitle() {
       if (this.ifDesigner()) {
         this.getTitle = this.value;
-        return
+        return;
       }
       if (!this.value && !this.code) return '';
       // 有转换器的时候，需要用 code
-      const tcode = this.value && this.converter !== 'name' ? this.value : this.code;
-      const provincet = this.getListTempNew('province', tcode.slice(0, 2) + '0000');
+      const tcode =
+        this.value && this.converter !== 'name' ? this.value : this.code;
+      const provincet = this.getListTempNew(
+        'province',
+        tcode.slice(0, 2) + '0000'
+      );
       const cityt = this.getListTempNew('city', tcode.slice(0, 4) + '00');
       const countyt = this.getListTempNew('county', tcode.slice(0, 6));
 
-      this.getTitle = [
-        provincet,
-        cityt,
-        countyt,
-      ].filter(item => !!item).join('/');
+      this.getTitle = [provincet, cityt, countyt]
+        .filter((item) => !!item)
+        .join('/');
     },
     togglePopup() {
       this.valuepopup = !this.valuepopup;
@@ -163,10 +205,10 @@ export default createComponent({
       if (this.converter === 'json') {
         try {
           if (value === null || value === undefined) return {};
-          if(typeof value === 'string') return JSON.parse(value || '{}');
-          if(typeof value === 'object') return value;
+          if (typeof value === 'string') return JSON.parse(value || '{}');
+          if (typeof value === 'object') return value;
         } catch (err) {
-            return {};
+          return {};
         }
       } else {
         return value || {};
@@ -259,6 +301,13 @@ export default createComponent({
       this.$emit('change', picker, parsedValues, index);
     },
 
+    confirm() {
+      // tirgger picker confirm
+      if (typeof this.$refs?.picker?.confirm === 'function') {
+        this.$refs?.picker?.confirm();
+      }
+    },
+
     onConfirm(values, index) {
       values = this.parseOutputValues(values);
       this.setValues();
@@ -273,6 +322,14 @@ export default createComponent({
       this.togglePopup();
       this.setTitle();
     },
+
+    cancel() {
+      // tirgger picker cancel
+      if (typeof this.$refs?.picker?.cancel === 'function') {
+        this.$refs?.picker?.cancel();
+      }
+    },
+
     onCancel() {
       this.togglePopup();
       this.code = this.oldcode;
@@ -295,9 +352,10 @@ export default createComponent({
       return '';
     },
 
-    setValues() {
+    setValues(isForce = false) {
       let { code } = this;
-      if(this.ifDesigner()) return;
+      // readme: 现在要在编辑态显示出来
+      if (!isForce && this.ifDesigner()) return;
       if (!code) {
         code = this.getDefaultCode();
       }
@@ -376,34 +434,89 @@ export default createComponent({
       this.setValues();
     },
     getListTempNew(type, code) {
-      return (this.areaList[`${type}_list`][code]);
+      return this.areaList[`${type}_list`][code];
     },
     convertName(names) {
       if (this.converter === 'name' && names) {
-        const nameArr = names.split('/').map(item => item.trim())
-        const levelMap = ['province', 'city', 'county']
+        const nameArr = names.split('/').map((item) => item.trim());
+        const levelMap = ['province', 'city', 'county'];
 
         function search(level, prefixCode) {
-          if (level >= nameArr.length) return prefixCode
-          for (const [code, name] of Object.entries(areaList[`${levelMap[level]}_list`])) {
+          if (level >= nameArr.length) return prefixCode;
+          for (const [code, name] of Object.entries(
+            areaList[`${levelMap[level]}_list`]
+          )) {
             if (code.startsWith(prefixCode) && name === nameArr[level]) {
-              return level === nameArr.length - 1 ?
-                code : search(level + 1, code.substring(0, (level +  1) * 2))
+              return level === nameArr.length - 1
+                ? code
+                : search(level + 1, code.substring(0, (level + 1) * 2));
             }
           }
         }
 
-        const res = search(0, '')
-        return res
+        const res = search(0, '');
+        return res;
       }
-      return names
+      return names;
     },
     convertCode(code) {
       if (this.converter === 'name') {
-        this.setTitle()
-        return this.getTitle
+        this.setTitle();
+        return this.getTitle;
       }
-      return code
+      return code;
+    },
+
+    genColumnsTopForNew() {
+      let topSlot = this.slots('picker-top');
+      let titleSlot = this.slots('pannel-title');
+      if (this.inDesigner()) {
+        if (!topSlot) {
+          topSlot = <EmptyCol></EmptyCol>;
+        }
+        if (!titleSlot) {
+          titleSlot = <EmptyCol></EmptyCol>;
+        }
+      }
+      return (
+        <template slot="columns-top">
+          <div class={bem('picker-top')}>
+            {topSlot && (
+              <div
+                vusion-slot-name="picker-top"
+                style="display:flex; justify-content: space-between; align-items: center; min-height:32px;"
+              >
+                {topSlot}
+              </div>
+            )}
+            <div
+              style="position:absolute; top: 50%; left:50%; transform: translate(-50%,-50%);"
+              vusion-slot-name="pannel-title"
+            >
+              {titleSlot || this.title}
+            </div>
+          </div>
+        </template>
+      );
+    },
+
+    genColumnsBottomForNew() {
+      let bottomSlot = this.slots('picker-bottom');
+      if (this.inDesigner()) {
+        if (!bottomSlot) {
+          bottomSlot = <EmptyCol></EmptyCol>;
+        }
+      }
+
+      if (!bottomSlot) return null;
+
+      return (
+        <template slot="columns-bottom">
+          <div vusion-slot-name="picker-bottom" class={bem('picker-bottom')}>
+            {bottomSlot}
+          </div>
+        </template>
+      );
     },
   },
 
@@ -414,7 +527,61 @@ export default createComponent({
       confirm: this.onConfirm,
     };
     const tempSlot = {
-      title: () => this.slots('title')
+      title: () => this.slots('title'),
+    };
+
+    let children = null;
+    if (this.isNew) {
+      children = (
+        <Picker
+          ref="picker"
+          class={bem()}
+          showToolbar
+          valueKey="name"
+          toolbarPosition="none"
+          title={this.title}
+          columns={this.displayColumns}
+          columnsprop={this.displayColumns}
+          loading={this.loading}
+          readonly={this.readonly}
+          itemHeight={this.itemHeight}
+          swipeDuration={this.swipeDuration}
+          visibleItemCount={this.visibleItemCount}
+          cancelButtonText={this.cancelButtonText}
+          confirmButtonText={this.confirmButtonText}
+          onCancel={this.onCancel}
+          {...{ on }}
+        >
+          {this.genColumnsTopForNew()}
+          {this.genColumnsBottomForNew()}
+        </Picker>
+      );
+    } else {
+      children = (
+        <Picker
+          ref="picker"
+          class={bem()}
+          showToolbar
+          valueKey="name"
+          title={this.title}
+          columns={this.displayColumns}
+          columnsprop={this.displayColumns}
+          loading={this.loading}
+          readonly={this.readonly}
+          itemHeight={this.itemHeight}
+          swipeDuration={this.swipeDuration}
+          visibleItemCount={this.visibleItemCount}
+          cancelButtonText={this.cancelButtonText}
+          confirmButtonText={this.confirmButtonText}
+          onCancel={this.onCancel}
+          scopedSlots={pickSlots(this, [
+            'title',
+            'columns-top',
+            'columns-bottom',
+          ])}
+          {...{ on }}
+        />
+      );
     }
     return (
       <div class={bem('wrappparea')}>
@@ -440,30 +607,23 @@ export default createComponent({
           closeOnClickOverlay={this.closeOnClickOverlay}
           // onClickOverlay={this.togglePopup}
           get-container="body" // 放body下不易出现异常情况
+          vusion-scope-id={this?.$vnode?.context?.$options?._scopeId}
+          {...{
+            attrs: { ...this.$attrs, 'vusion-empty-background': undefined },
+          }}
         >
-          <Picker
-            ref="picker"
-            class={bem()}
-            showToolbar
-            valueKey="name"
-            title={this.title}
-            columns={this.displayColumns}
-            columnsprop={this.displayColumns}
-            loading={this.loading}
-            readonly={this.readonly}
-            itemHeight={this.itemHeight}
-            swipeDuration={this.swipeDuration}
-            visibleItemCount={this.visibleItemCount}
-            cancelButtonText={this.cancelButtonText}
-            confirmButtonText={this.confirmButtonText}
-            onCancel={this.onCancel}
-            scopedSlots={pickSlots(this, [
-              'title',
-              'columns-top',
-              'columns-bottom',
-            ])}
-            {...{ on }}
-          />
+          <div class={bem(this.isNew && 'content-wrapper')}>
+            {this.inDesigner() && (
+              <div
+                class={bem('designer-close-button')}
+                vusion-click-enabled="true"
+                onClick={this.designerClose}
+              >
+                点击关闭
+              </div>
+            )}
+            {children}
+          </div>
         </Popup>
       </div>
     );
