ef3e899911bc78d946e3bd7474796fc548966d94
御风 <18012261618@126.com>
2023-10-23 18:10:02 +0800

    feat: 初步开发完成

---
+++ src/capsules/demo/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <demo-section>
+    <demo-block title="normal">
+      <div>value: {{active}}</div>
+      <van-capsules :value.sync="active" :multiple="multiple" :cancelable="cancelable" @change="onChange">
+        <van-capsules-item value="1" disabled>标签</van-capsules-item>
+        <van-capsules-item value="2" flag>标签</van-capsules-item>
+        <van-capsules-item value="3" label="1群">标签</van-capsules-item>
+      </van-capsules>
+    </demo-block>
+
+    <demo-block title="normal group">
+      <van-capsules :value.sync="active" :multiple="multiple" :cancelable="cancelable" @change="onChange">
+        <van-capsules-group>
+          <van-capsules-item value="1" disabled>标签</van-capsules-item>
+          <van-capsules-item value="2" flag>标签</van-capsules-item>
+        </van-capsules-group>
+
+        <van-capsules-group>
+          <van-capsules-item value="3" label="1群">标签</van-capsules-item>
+          <van-capsules-item value="4" >标签</van-capsules-item>
+        </van-capsules-group>
+
+      </van-capsules>
+    </demo-block>
+
+    <demo-block title="designer">
+      <van-capsules :value.sync="active" :multiple="multiple" :cancelable="cancelable" @change="onChange">
+      </van-capsules>
+
+      <van-capsules :value.sync="active" :multiple="multiple" :cancelable="cancelable" @change="onChange">
+        <van-capsules-item value="1" disabled>标签</van-capsules-item>
+        <van-capsules-item value="2" flag></van-capsules-item>
+        <van-capsules-item value="3" label="1群">标签</van-capsules-item>
+      </van-capsules>
+    </demo-block>
+  </demo-section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: null,
+      multiple: false,
+      cancelable: false
+    };
+  },
+  methods: {
+    onChange(e) {
+      console.log('onChange', e.value);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import '../../style/var';
+
+</style>
--- src/mixins/slots.js
+++ src/mixins/slots.js
@@ -18,7 +18,8 @@ export const SlotsMixin = {
       // 开发态
       if (process.env.NODE_ENV !== 'production') {
         const searchParams = new URLSearchParams(window.location.search);
-        return searchParams.get('VUE_APP_DESIGNER') === 1;
+        // eslint-disable-next-line eqeqeq
+        return searchParams.get('VUE_APP_DESIGNER') == 1;
       }
 
       return this.$env && this.$env.VUE_APP_DESIGNER;
