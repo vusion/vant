a53f3babe39324d1991814c0546058f6412da96c
御风 <18012261618@126.com>
2023-11-29 15:38:34 +0800

    feat: 标签栏

--- src/tabbar/api.ts
+++ src/tabbar/api.ts
@@ -7,7 +7,6 @@ namespace nasl.ui {
         description: '标签栏',
     })
     export class VanTabbar extends VueComponent {
-
         constructor(options?: Partial<VanTabbarOptions>) { super(); }
     }
 
@@ -16,6 +15,7 @@ namespace nasl.ui {
             title: '值',
             description: '值',
             syncMode: 'both',
+            group: '数据属性'
         })
         value: nasl.core.String;
 
@@ -28,6 +28,7 @@ namespace nasl.ui {
         @Prop({
             title: '是否显示外边框',
             description: '是否显示外边框',
+            group: '样式属性'
         })
         border: nasl.core.Boolean = true;
 
@@ -41,7 +42,7 @@ namespace nasl.ui {
             title: '切换后',
             description: '切换标签时触发',
         })
-        onChange: () => void;
+        onChange: (value: nasl.core.String) => void;
 
         @Slot({
             title: 'undefined',
@@ -61,7 +62,6 @@ namespace nasl.ui {
         title: '标签项',
     })
     export class VanTabbarItem extends VueComponent {
-
         constructor(options?: Partial<VanTabbarItemOptions>) { super(); }
     }
 
@@ -69,6 +69,7 @@ namespace nasl.ui {
         @Prop({
             title: '标签项值',
             description: '标签项值',
+            group: '数据属性'
         })
         name: nasl.core.String;
 
@@ -84,24 +85,28 @@ namespace nasl.ui {
         @Prop({
             title: '是否显示徽章',
             description: '是否显示徽章',
+            group: '数据属性'
         })
         showbaget: nasl.core.Boolean = false;
 
         @Prop({
             title: '徽章值',
             description: '徽章值',
+            group: '数据属性'
         })
         badge: nasl.core.Integer;
 
         @Prop({
             title: '徽章最大值',
             description: '徽章最大值',
+            group: '数据属性'
         })
         badgemax: nasl.core.Integer;
 
         @Prop({
             title: '链接类型',
             description: '链接类型',
+            group: '交互属性',
             setter: {
                 type: 'enumSelect',
                 titles: ['页面跳转', '下载链接'],
@@ -112,12 +117,14 @@ namespace nasl.ui {
         @Prop({
             title: '链接',
             description: '链接地址',
+            group: '交互属性',
         })
         hrefAndTo: nasl.core.String;
 
         @Prop({
             title: '打开方式',
             description: '父级窗口和顶级窗口仅适用于iframe组件嵌套的情况，若不存在嵌套，则打开方式同当前窗口。',
+            group: '交互属性',
             setter: {
                 type: 'enumSelect',
                 titles: ['新窗口', '当前窗口', '父级窗口', '顶级窗口'],
@@ -129,6 +136,6 @@ namespace nasl.ui {
             title: '点击标签项',
             description: '点击选项导致 value 变化时触发',
         })
-        onClick: () => void;
+        onClick: (event: nasl.ui.MouseEvent) => void;
     }
 }
