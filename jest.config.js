7bcc2757ee6c0e40a29765efc0d44e1b6424408c
YufJi <18012261618@126.com>
2024-01-13 05:11:46 +0800

    test: 💍 更新snapshot

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': '<rootDir>/test/config/jest.style-mock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/config/jest.file-mock.js',
+
+    // iconv/icon.js is a mock file
+    '\\/icon(\\.js)?': '<rootDir>/test/config/jest.icon-mock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/test/config/jest.setup.js'],
+
+  moduleFileExtensions: ['js', 'jsx', 'vue', 'ts', 'tsx'],
+
+  transform: {
+    '\\.(vue)$': 'vue-jest',
+    '\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(@vant/cli))/', 'api.ts'],
+  snapshotSerializers: ['jest-serializer-vue'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx,vue}',
+    '!**/demo/**',
+    '!src/**/api.ts',
+  ],
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+  coverageDirectory: './test/coverage',
+};
